560385c3cc1c46a5360fc13f928bf2fe0e02a92a
Allan Roque Barbosa da Silva <allan.barbosa@servicenet.com.br>
2021-05-13 00:14:56 -0300

    Refatoração

--- src/app/shared/components/sidebar/sidebar.component.ts
+++ src/app/shared/components/sidebar/sidebar.component.ts
@@ -7,13 +7,10 @@ import { environment } from 'src/environments/environment';
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.scss']
 })
-export class SidebarComponent implements OnInit {
+export class SidebarComponent {
   appName: string = environment.appName;
 
   constructor(private _router: Router) { }
-
-  ngOnInit(): void {
-  }
 
   navigateTo(route: string) {
     this._router.navigate([route])
