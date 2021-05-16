80b16810010eee4d828cb65b871c9a5432f974fd
Allan Roque Barbosa da Silva <allan.barbosa@servicenet.com.br>
2021-05-16 12:46:26 -0300

    Ajustando filtro apenas para placa

--- src/app/shared/api/services/alelo-frota.service.ts
+++ src/app/shared/api/services/alelo-frota.service.ts
@@ -8,99 +8,117 @@ import { map as __map, filter as __filter } from 'rxjs/operators';
 import { Car } from '../models/car';
 
 @Injectable({
-    providedIn: 'root',
+  providedIn: 'root',
 })
 class AleloFrotaService extends __BaseService {
-    constructor(
-        config: __Configuration,
-        http: HttpClient
-    ) {
-        super(config, http);
-    }
+  constructor(config: __Configuration, http: HttpClient) {
+    super(config, http);
+  }
 
-    carsUsingGET(params?: AleloFrotaService.FilterParams): __Observable<Car[]> {
-        return this.carsUsingGETResponse(params).pipe(
-            __map(_r => _r.body as Car[])
-        );
-    }
+  carsUsingGET(params?: AleloFrotaService.FilterParams): __Observable<Car[]> {
+    return this.carsUsingGETResponse(params).pipe(
+      __map((_r) => _r.body as Car[])
+    );
+  }
 
-    carsUsingGETResponse(params?: AleloFrotaService.FilterParams): __Observable<__StrictHttpResponse<Car[]>> {
-        let url = this.rootUrl + `vehicle?${params?.filter ? 'filter=' + params.filter : ''}${params?.page && params?.limit ? '&page=' + params.page + '&limit=' + params.limit : ''}`
+  carsUsingGETResponse(
+    params?: AleloFrotaService.FilterParams
+  ): __Observable<__StrictHttpResponse<Car[]>> {
+    let url =
+      this.rootUrl +
+      `vehicle?${params?.filter ? 'plate=' + params.filter : ''}${
+        params?.page && params?.limit
+          ? '&page=' + params.page + '&limit=' + params.limit
+          : ''
+      }`;
 
-        let req = new HttpRequest<any>('GET', url);
+    let req = new HttpRequest<any>('GET', url);
 
-        return this.http.request<any>(req).pipe(
-            __filter(_r => _r instanceof HttpResponse),
-            __map((_r) => _r as __StrictHttpResponse<Car[]>)
-        );
-    }
+    return this.http.request<any>(req).pipe(
+      __filter((_r) => _r instanceof HttpResponse),
+      __map((_r) => _r as __StrictHttpResponse<Car[]>)
+    );
+  }
 
-    carUsingGET(params: AleloFrotaService.FindCarParams): __Observable<Car> {
-        return this.carUsingGETResponse(params).pipe(
-            __map(_r => _r.body as Car)
-        );
-    }
+  carUsingGET(params: AleloFrotaService.FindCarParams): __Observable<Car> {
+    return this.carUsingGETResponse(params).pipe(__map((_r) => _r.body as Car));
+  }
 
-    carUsingGETResponse(params: AleloFrotaService.FindCarParams): __Observable<__StrictHttpResponse<Car>> {
-        let req = new HttpRequest<any>('GET', this.rootUrl + `/vehicle/${params.id}`);
+  carUsingGETResponse(
+    params: AleloFrotaService.FindCarParams
+  ): __Observable<__StrictHttpResponse<Car>> {
+    let req = new HttpRequest<any>(
+      'GET',
+      this.rootUrl + `/vehicle/${params.id}`
+    );
 
-        return this.http.request<any>(req).pipe(
-            __filter(_r => _r instanceof HttpResponse),
-            __map((_r) => _r as __StrictHttpResponse<Car>)
-        );
-    }
+    return this.http.request<any>(req).pipe(
+      __filter((_r) => _r instanceof HttpResponse),
+      __map((_r) => _r as __StrictHttpResponse<Car>)
+    );
+  }
 
-    carUsingPOSTOrPUT(params: AleloFrotaService.SaveCarParams): __Observable<Car> {
-        return this.carUsingPOSTOrPUTResponse(params).pipe(
-            __map(_r => _r.body as Car)
-        );
-    }
+  carUsingPOSTOrPUT(
+    params: AleloFrotaService.SaveCarParams
+  ): __Observable<Car> {
+    return this.carUsingPOSTOrPUTResponse(params).pipe(
+      __map((_r) => _r.body as Car)
+    );
+  }
 
-    carUsingPOSTOrPUTResponse(params: AleloFrotaService.SaveCarParams): __Observable<__StrictHttpResponse<Car>> {
-        let __body = params.car;
-        let __method = __body.id ? 'PUT' : 'POST';
+  carUsingPOSTOrPUTResponse(
+    params: AleloFrotaService.SaveCarParams
+  ): __Observable<__StrictHttpResponse<Car>> {
+    let __body = params.car;
+    let __method = __body.id ? 'PUT' : 'POST';
 
-        let req = new HttpRequest<any>(
-            __method,
-            this.rootUrl + `/vehicle/${__body.id || ''}`,
-            __body);
+    let req = new HttpRequest<any>(
+      __method,
+      this.rootUrl + `/vehicle/${__body.id || ''}`,
+      __body
+    );
 
-        return this.http.request<any>(req).pipe(
-            __filter(_r => _r instanceof HttpResponse),
-            __map((_r) => _r as __StrictHttpResponse<Car>)
-        );
-    }
+    return this.http.request<any>(req).pipe(
+      __filter((_r) => _r instanceof HttpResponse),
+      __map((_r) => _r as __StrictHttpResponse<Car>)
+    );
+  }
 
-    carUsingDELETE(params: AleloFrotaService.FindCarParams): __Observable<Car> {
-        return this.carUsingDELETEResponse(params).pipe(
-            __map(_r => _r.body as Car)
-        );
-    }
+  carUsingDELETE(params: AleloFrotaService.FindCarParams): __Observable<Car> {
+    return this.carUsingDELETEResponse(params).pipe(
+      __map((_r) => _r.body as Car)
+    );
+  }
 
-    carUsingDELETEResponse(params: AleloFrotaService.FindCarParams): __Observable<__StrictHttpResponse<Car>> {
-        let req = new HttpRequest<any>('DELETE', this.rootUrl + `/vehicle/${params.id}`);
+  carUsingDELETEResponse(
+    params: AleloFrotaService.FindCarParams
+  ): __Observable<__StrictHttpResponse<Car>> {
+    let req = new HttpRequest<any>(
+      'DELETE',
+      this.rootUrl + `/vehicle/${params.id}`
+    );
 
-        return this.http.request<any>(req).pipe(
-            __filter(_r => _r instanceof HttpResponse),
-            __map((_r) => _r as __StrictHttpResponse<Car>)
-        );
-    }
+    return this.http.request<any>(req).pipe(
+      __filter((_r) => _r instanceof HttpResponse),
+      __map((_r) => _r as __StrictHttpResponse<Car>)
+    );
+  }
 }
 
 module AleloFrotaService {
-    export interface FilterParams {
-        filter?: string;
-        page?: number;
-        limit?: number;
-    }
+  export interface FilterParams {
+    filter?: string;
+    page?: number;
+    limit?: number;
+  }
 
-    export interface FindCarParams {
-        id: number
-    }
+  export interface FindCarParams {
+    id: number;
+  }
 
-    export interface SaveCarParams {
-        car: Car
-    }
+  export interface SaveCarParams {
+    car: Car;
+  }
 }
 
-export { AleloFrotaService }
+export { AleloFrotaService };
